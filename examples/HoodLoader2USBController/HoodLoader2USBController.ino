857c7c5e635242dd06a18b2611031fc4955bc5ea
Legion2 <leon.k97@gmx.de>
2019-03-17 15:10:34 +0100

    added examples for HoodLoader bridge #9

---
+++ examples/HoodLoader2USBController/HoodLoader2USBController.ino
@@ -0,0 +1,40 @@
+/*
+   Copyright 2019 Leon Kiefer
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+#include <CorsairLightingNodePRO.h>
+#include "SerialLEDController.h"
+
+SerialLEDController ledController;
+CorsairLightingProtocol cLP(&ledController, firmware_version);
+
+
+void setup() {
+#ifdef DEBUG
+	Serial.begin(115200);
+#endif
+	Serial1.begin(115200);
+	cLP.begin();
+}
+
+void loop() {
+	if (cLP.available())
+	{
+		Command command;
+		cLP.getCommand(command);
+		cLP.handleCommand(command);
+	}
+
+	ledController.handleCallback();
+}
---
+++ examples/HoodLoader2USBController/SerialLEDController.h
@@ -0,0 +1,32 @@
+/*
+   Copyright 2019 Leon Kiefer
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+#ifndef _SERIALLEDCONTROLLER_h
+#define _SERIALLEDCONTROLLER_h
+
+#include <ILEDController.h>
+
+class SerialLEDController : public ILEDController {
+public:
+	virtual void handleLEDControl(const Command& command, const CorsairLightingProtocolResponse* clp) override;
+	virtual void handleCallback();
+private:
+	const CorsairLightingProtocolResponse* response;
+	byte buff[RESPONSE_SIZE];
+	uint8_t part = 0;
+};
+
+#endif
+
