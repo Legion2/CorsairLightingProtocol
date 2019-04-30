35482635a78561ad372e48458185291f086199b8
Legion2 <leon.k97@gmx.de>
2019-04-30 22:14:42 +0200

    improved example with comments added disable built in leds

--- examples/SimpleLightingController/SimpleLightingController.ino
+++ examples/SimpleLightingController/SimpleLightingController.ino
@@ -13,26 +13,50 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-#include <ILEDController.h>
 #include <CorsairLightingProtocol.h>
 #include <LEDController.h>
 #include <FastLED.h>
 
-// iCUE changes to a very slow mode when there are more than 50 leds per channel
+// The number of LEDs per channel, there are two channels.
+// iCUE changes to a very slow mode when there are more than 50 LEDs per channel.
 #define CHANNEL_LED_COUNT 50
-#define NUM_LEDS CHANNEL_LED_COUNT * 2
+
+// Total count of LEDs on all channels, the value is calculated based on the leds per channel.
+#define NUM_LEDS (CHANNEL_LED_COUNT * 2)
+
+// The Arduino pin where the physical LEDs are connected.
+// You can use two pins, one for each channel.
+// In this example we use only one pin where both channel are connected in series.
 #define DATA_PIN 2
 
-LEDController<CHANNEL_LED_COUNT> ledController(true);
+// Should the EEPROM of the Arduino be used to store persistent information like the Hardware Lighting.
+// If enabled, the Hardware Lighting configured in iCUE works without a USB connection and even after a restart of the Arduino.
+#define USE_EEPROM true
+
+// This will disable the RX and TX built in leds on Arduino Leonardo, Micro and Pro Micro.
+#define DISABLE_BUILTIN_LED
+
+LEDController<CHANNEL_LED_COUNT> ledController(USE_EEPROM);
 CorsairLightingProtocol cLP(&ledController);
 
+// This array conatins all RGB values for all LEDs of the both channels.
 CRGB leds[NUM_LEDS];
 
+// This variable stores the current command which was received.
 Command command;
 
 void setup() {
 #ifdef DEBUG
 	Serial.begin(115200);
+#else
+#ifdef DISABLE_BUILTIN_LED
+#ifdef LED_BUILTIN_RX
+	pinMode(LED_BUILTIN_RX, INPUT);
+#endif
+#ifdef LED_BUILTIN_TX
+	pinMode(LED_BUILTIN_TX, INPUT);
+#endif
+#endif
 #endif
 	FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 	ledController.addLeds(0, leds);
