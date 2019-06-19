5fd99041970e8d3221f8f5eb2c8b740cdb9dc945
Legion2 <leon.k97@gmx.de>
2019-06-20 00:03:18 +0200

    set higher PWM frequency for fans

--- examples/CommanderPRO/CommanderPRO.ino
+++ examples/CommanderPRO/CommanderPRO.ino
@@ -22,10 +22,14 @@
 #define DATA_PIN_CHANNEL_1 2
 #define DATA_PIN_CHANNEL_2 3
 
-#define TEMP_SENSOR_PIN_1 A7
+#define TEMP_SENSOR_PIN_1 A6
+#define TEMP_SENSOR_PIN_2 A8
 
 #define FAN_UPDATE_RATE 500
-#define PWM_FAN_PIN_1 4
+#define PWM_FAN_PIN_1 5
+#define PWM_FAN_PIN_2 6
+#define PWM_FAN_PIN_3 9
+#define PWM_FAN_PIN_4 10
 
 #define CHANNEL_LED_COUNT 60
 
@@ -38,6 +42,9 @@ CRGB ledsChannel1[CHANNEL_LED_COUNT];
 CRGB ledsChannel2[CHANNEL_LED_COUNT];
 
 PWMFan fan1(PWM_FAN_PIN_1, 0, 2000);
+PWMFan fan2(PWM_FAN_PIN_2, 0, 2000);
+PWMFan fan3(PWM_FAN_PIN_3, 0, 2000);
+PWMFan fan4(PWM_FAN_PIN_4, 0, 2000);
 
 Command command;
 
@@ -53,8 +60,11 @@ void setup() {
 	ledController.addLeds(0, ledsChannel1);
 	ledController.addLeds(1, ledsChannel2);
 	temperatureController.addSensor(0, TEMP_SENSOR_PIN_1);
-	temperatureController.addSensor(1, TEMP_SENSOR_PIN_1);
+	temperatureController.addSensor(1, TEMP_SENSOR_PIN_2);
 	fanController.addFan(0, &fan1);
+	fanController.addFan(1, &fan2);
+	fanController.addFan(2, &fan3);
+	fanController.addFan(3, &fan4);
 	cLP.begin();
 }
 
--- examples/CommanderPRO/PWMFan.cpp
+++ examples/CommanderPRO/PWMFan.cpp
@@ -19,6 +19,32 @@ PWMFan::PWMFan(uint8_t pwmPin, uint16_t minRPM ,uint16_t maxRPM) : pwmPin(pwmPin
 {
 	pinMode(pwmPin, OUTPUT);
 	analogWrite(pwmPin, 0);
+	switch (digitalPinToTimer(pwmPin)) {
+	case TIMER0B:/* 3 */
+#ifdef DEBUG
+		Serial.println(F("Pin not supported as PWM fan pin"));
+		Serial.println(F("We don't want to mess up arduino time functions"));
+#endif // DEBUG
+		break;
+	case TIMER3A:/* 5 */
+		TCCR3B = (TCCR3B & B11111000) | 0x01;
+		break;
+	case TIMER4D:/* 6 */
+		//PLLFRQ = (PLLFRQ & B11001111) | (0x03 << PLLTM0);
+		TCCR4B = (TCCR4B & B11110000) | 0x01;
+		break;
+	case TIMER1A:/* 9 */
+		TCCR1B = (TCCR1B & B11111000) | 0x01;
+		break;
+	case TIMER1B:/* 10 */
+		TCCR1B = (TCCR1B & B11111000) | 0x01;
+		break;
+	default:
+#ifdef DEBUG
+		Serial.println(F("Pin not supported as PWM fan pin"));
+#endif // DEBUG
+		break;
+	}
 }
 
 void PWMFan::setPower(uint8_t percentage)
