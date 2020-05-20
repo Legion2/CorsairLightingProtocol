d11830e2d69525f42e57ddf95a55d411027547d2
Legion2 <Legion2@users.noreply.github.com>
2020-05-20 18:47:26 +0000

    Automated deployment: Wed May 20 18:47:26 UTC 2020 ed18054a963aaa654ed161b0a536a3d8766c8723

--- navtreedata.js
+++ navtreedata.js
@@ -1,7 +1,6 @@
 var NAVTREE =
 [
   [ "Corsair Lighting Protocol", "index.html", [
-    [ "Corsair Lighting Protocol [![arduino-library-badge](https://www.ardu-badge.com/badge/Corsair%20Lighting%20Protocol.svg?)](https://www.ardu-badge.com/Corsair%20Lighting%20Protocol) [![Test Status](https://github.com/Legion2/CorsairLightingProtocol/workflows/Test/badge.svg)](https://github.com/Legion2/CorsairLightingProtocol/actions?query=workflow%3ATest+branch%3Adev+event%3Apush)", "index.html", null ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['corsair_20lighting_20protocol_20_5b_21_5barduino_2dlibrary_2dbadge_5d_28https_3a_2f_2fwww_2eardu_2dbadge_2ecom_2fbadge_2fcorsair_2520lighting_2520protocol_2esvg_3f_29_5d_28https_3a_2f_2fwww_2eardu_2dbadge_2ecom_2fcorsair_2520lighting_2520protocol_29_20_5b_21_5btest_20status_5d_28https_3a_2f_2fgithub_2ecom_2flegion2_2fcorsairlightingprotocol_2fworkflows_2ftest_2fbadge_2esvg_29_5d_28https_3a_2f_2fgithub_2ecom_2flegion2_2fcorsairlightingprotocol_2factions_3fquery_3dworkflow_253atest_2bbranch_253adev_2bevent_253apush_29',['Corsair Lighting Protocol [![arduino-library-badge](https://www.ardu-badge.com/badge/Corsair%20Lighting%20Protocol.svg?)](https://www.ardu-badge.com/Corsair%20Lighting%20Protocol) [![Test Status](https://github.com/Legion2/CorsairLightingProtocol/workflows/Test/badge.svg)](https://github.com/Legion2/CorsairLightingProtocol/actions?query=workflow%3ATest+branch%3Adev+event%3Apush)',['../index.html',1,'']]]
+  ['corsair_20lighting_20protocol',['Corsair Lighting Protocol',['../index.html',1,'']]]
 ];
